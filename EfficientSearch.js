d0bbd1e887c9eb642b1e3ac0eb8df4e3bb60b4cf
Sean Nealon <signalflowsean@gmail.com>
2018-12-11 11:51:26 -0500

    EfficientSearch.js

---
+++ EfficientSearch.js
@@ -0,0 +1,25 @@
+function efficientSearch(array, item) {
+    let minIndex = 0;
+    let maxIndex = array.length - 1;
+    let currentIndex;
+    let currentElement;
+
+    while (minIndex <= maxIndex) {
+        //algorithm uses subsets!
+        currentIndex = Math.floor((minIndex + maxIndex) / 2);
+        currentElement = array[currentIndex];
+
+        if (currentElement < item) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > item) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return -1;
+}
+
+//O(log(n))
